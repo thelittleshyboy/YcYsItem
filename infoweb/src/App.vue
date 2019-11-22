5f43f3e7cd28d3f2bc03f59ae5e9d768b6a2e7c9
123 <123@qq.com>
2019-11-23 00:06:04 +0800

    主页样式

--- infoweb/src/App.vue
+++ infoweb/src/App.vue
@@ -1,10 +1,52 @@
 <template>
   <div id="app">
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="180px">
+        <el-button type="text" style="margin-left:50px;color:#ffd04b">登录&nbsp;&nbsp;</el-button>|<el-button type="text" style="color:#ffd04b">注册</el-button>
+        <el-row>
+          <el-col :span="24">
+            <el-menu
+              default-active="1"
+              class="el-menu-vertical-demo"
+              background-color="#444444"
+              text-color="#fff"
+              active-text-color="#ffd04b"
+            >
+              <el-menu-item index="1">
+                <i class="el-icon-menu"></i>
+                <span slot="title">主页</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <i class="el-icon-document"></i>
+                <span slot="title">我的</span>
+              </el-menu-item>
+              <el-submenu index="2">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>分类</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="1-1">全部</el-menu-item>
+                  <el-menu-item index="1-2">电影</el-menu-item>
+                  <el-menu-item index="1-3">小说</el-menu-item>
+                  <el-menu-item index="1-4">游戏</el-menu-item>
+                  <el-menu-item index="1-5">美食</el-menu-item>
+                  <el-menu-item index="1-6">丽人</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+              <el-menu-item index="3">
+                <i class="el-icon-setting"></i>
+                <span slot="title">管理</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
       <el-container>
         <el-header height="80px">Header</el-header>
-        <el-main>Main</el-main>
+        <el-main>
+          <router-view />
+        </el-main>
       </el-container>
     </el-container>
   </div>
@@ -12,16 +54,22 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  mounted() {
+    console.log(this.$route)
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.el-container .el-aside {
+  padding-top: 20px;
 }
 </style>
---
+++ infoweb/src/components/Home.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+ <div class="search-input">
+  <el-input placeholder="请输入内容" v-model="input">
+    <el-button slot="append" icon="el-icon-search"></el-button>
+  </el-input>
+</div>
+    <el-carousel :interval="3000" type="card" height="300px">
+      <el-carousel-item v-for="item in imgList" :key="item.id">
+        <img :src="item.idView" style="width:823.5px;height:300px">
+      </el-carousel-item>
+    </el-carousel>
+  <h2>精选推荐</h2>
+  <el-divider></el-divider>
+  <div class="card-list-outside">
+    <div class="card-list" v-for="(o, index) in 5" :key="index">
+    <el-card class="card-style">
+      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+      <div>
+        <span>好吃的汉堡</span>
+        <el-button type="text" class="button">详情</el-button>
+      </div>
+    </el-card>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      imgList: [{
+        id: '1',
+        idView: require('../assets/carouselImg1.jpg')
+      }, {
+        id: '2',
+        idView: require('../assets/carouselImg2.jpg')
+      }, {
+        id: '3',
+        idView: require('../assets/carouselImg3.jpg')
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+  
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    height:200px;
+    display: block;
+  }
+
+  .card-list-outside {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  .card-list {
+    margin-top: 10px;
+    margin-left: 10px;
+    width: 350px;
+  }
+
+  .card-style {
+    width: 300px;
+    height: 400px;
+  }
+
+  .search-input {
+    display: inline-block;
+    position: absolute;
+    left: 45%;
+    top: 300px;
+    z-index: 99;
+  }
+  .search-input .el-input {
+    width: 400px
+  }
+</style>
