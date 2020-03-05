0b009f6e3ccb881e1f88fdf328678184a661ec1d
123 <123@qq.com>
2020-03-05 17:23:30 +0800

    fix：修复bug,加入话题界面

--- infoweb/src/components/Classify/Movie.vue
+++ infoweb/src/components/Classify/Movie.vue
@@ -1,26 +1,186 @@
 <template>
-  <div id="app">
-      movie
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="17" :offset="1">
+        <div class="topic-content">
+          <el-row v-for="(item, index) in myList" :key="index" class="details-info">
+            <router-link :to="{name:'DetailsArticle',params:{id:item._id}}">
+              <el-col :span="6">
+                <div class="demo-image__preview">
+                  <el-image
+                    style="width: 100%; height: 200px;"
+                    :src="item.cover ? 'http://'+item.cover : 'https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg'"
+                  ></el-image>
+                </div>
+              </el-col>
+            </router-link>
+            <el-col :span="18">
+              <el-row>
+                <el-col :span="14">
+                  <span>#{{ item.region }}#</span>
+                  <h2
+                    style="float:left;margin-left:25px;color:black"
+                  >{{ item ? item.title : '' | ellipsis(20) }}</h2>
+                </el-col>
+                <el-col :span="10">
+                  <el-row style="margin-top:10px;float:right;margin-right:20px">
+                    <el-button type="primary" icon="el-icon-back" circle @click="handleWatch(item)"></el-button>
+                  </el-row>
+                </el-col>
+              </el-row>
+              <el-row>
+                <el-col
+                  :span="23"
+                  style="overflow:hidden;margin-left:25px;color:black"
+                >{{ item ? item.desc : '' | ellipsis(300) }}</el-col>
+              </el-row>
+              <el-row>
+                <el-rate
+                  v-model="rate"
+                  disabled
+                  show-score
+                  text-color="#ff9900"
+                  score-template="{value}"
+                  style="float:right;margin-right:25px;margin-top:10px"
+                ></el-rate>
+              </el-row>
+            </el-col>
+          </el-row>
+          <el-pagination
+            background
+            layout="prev, pager, next, jumper, slot"
+            :total="totalNum"
+            :page-size="pageSize"
+            :current-page="page"
+            @current-change="current_change"
+            style="margin-top:20px"
+          ></el-pagination>
+        </div>
+      </el-col>
+      <el-col :span="5">
+        <div class="topic-right">
+          <div class="topic-right-text">
+            <h1>话题</h1>
+          </div>
+          <div class="topic-right-header">
+            <el-input placeholder="请输入文章标题搜索" v-model="searchValue" class="search-input">
+              <el-button slot="append" icon="el-icon-search" @click="searchTopic"></el-button>
+            </el-input>
+          </div>
+          <div class="topic-right-hot">
+            <h2>最新话题</h2>
+            <div v-for="(item, index) in topicNewList" :key="index">
+              <div style="color: #333;margin-top:5px">#{{ item.topicName }}</div>
+            </div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
+import { getAllList } from '../../api/article'
+import { newTopic } from '../../api/topic'
+
 export default {
   name: 'App',
-  mounted() {
-    console.log(this.$route)
+  data() {
+    return {
+      myList: [],
+      topicNewList: [],
+      page: 1,
+      totalNum: 0,
+      pageSize: 0,
+      rate: 3.7,
+      searchValue: null
+    }
   },
-  computed: {
+  mounted() {
+    this.getAllList()
+    this.newTopic()
+  },
+  methods: {
+    newTopic() {
+      newTopic().then(res => {
+        if (res.data.status === 'success') {
+          this.topicNewList = res.data.data
+        }
+      }), err => {
+        console.log(err)
+      }
+    },
+    searchTopic() {
+
+    },
+    handleWatch(item) {
+      this.$router.push('/details/' + item._id)
+    },
+    current_change(index) {
+      this.page = index
+      this.getAllList()
+    },
+    getAllList() {
+      getAllList({ title: this.searchValue, page: this.page }).then(res => {
+        if (res.data.status === 'success') {
+          this.myList = res.data.data
+          this.pageSize = res.data.pageSize
+          this.totalNum = res.data.total
+        }
+      }), err => {
+      }
+    },
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.el-main {
+  overflow: hidden;
+}
+.el-row {
+  margin-top: 20px;
+}
+.topic-content {
+  width: 100%;
+  padding-bottom: 20px;
+  padding-top: 20px;
+}
+.topic-right {
+  margin-top: 30px;
+  width: 100%;
+}
+.topic-right-text {
   text-align: center;
-  color: #2c3e50;
+  font-size: 30px;
+  height: 50px;
+}
+.topic-right-header {
+  background: #f8f8f8;
+  padding: 5px;
+  min-height: 200px;
+  margin-top: 5px;
+  text-align: center;
+  background-image: url("../../assets/backSearch.jpg");
+  background-size: 100% 100%;
+}
+.search-input {
+  margin-top: 20px;
+  margin-right: 10%;
+  width: 80%;
+}
+.topic-right-hot {
+  background: #f2f2f5;
+  margin-top: 20px;
+  border-top: 1px solid #fa2f2f;
+  margin-bottom: 20px;
+  min-height: 500px;
+  text-align: center;
+}
+.details-info {
+  border: 1px solid #e0e0e0;
+  margin-top: 10px;
+  line-height: 20px;
+  text-align: left;
 }
 </style>
